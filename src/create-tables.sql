2b74eb9df2ee51e47de61642dfd4ead4aba0be5a
Maxim <mamode12@yandex.com>
2021-09-20 13:27:08 +0300

    Constraints added.

--- src/create-tables.sql
+++ src/create-tables.sql
@@ -1,10 +1,13 @@
 create table radar
 (
     serial_number int primary key,
-    ship_name varchar(16),
-    period integer,
+    ship_name varchar(16)
+        constraint not_empty_name check (len(ship_name) > 4),
+    period integer
+        constraint positive_period check (period > 0),
     explore_start_date date
-);
+        constraint valid_explore_start_date check (EXTRACT(YEAR FROM explore_start_date) > 2021)
+    );
 
 create table coordinates
 (
@@ -16,22 +19,25 @@ create table coordinates
 create table signal_params
 (
     id serial primary key,
-    intensity numeric not null,
-    frequency numeric not null,
+    intensity numeric not null
+        constraint positive_intensity check (intensity > 0),
+    frequency numeric not null
+        constraint positive_frequency check (frequency > 0),
     amplitude numeric not null
+        constraint positive_amplitude check (amplitude > 0)
 );
 
 create table composition
 (
     id serial primary key,
-    description text not null
+    description text not null constraint not_empty_description check (len(description) > 3)
 );
 
 create table signal
 (
     id serial primary key,
     sent_time timestamp not null,
-    radar_serial_number int references radar (serial_number),
+    radar_serial_number int references radar (serial_number) not null ,
     sent_coords_id int references coordinates (id) not null,
     params_id int references signal_params (id) not null
 );
@@ -39,7 +45,7 @@ create table signal
 create table astro_body
 (
     code_name serial primary key,
-    mass int,
+    mass int constraint positive_mass check (mass > 0),
     coords_id int references coordinates (id) not null,
     composition_id int references composition (id) not null
 );
@@ -48,6 +54,6 @@ create table discovered_astro_bodies
 (
     id                   serial primary key,
     discovered_time      timestamp not null,
-    signal_id            int references signal (id),
+    signal_id            int references signal (id) not null,
     astro_body_code_name int references astro_body (code_name) not null
 );
